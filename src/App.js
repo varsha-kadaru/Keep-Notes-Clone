8485116e78eadce76651300e6ddb5498755c3b6e
varsha-kadaru <107875699+varsha-kadaru@users.noreply.github.com>
2023-04-30 16:32:02 +0530

    Keep Notes

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import React, { useState } from "react";
+import Header from "./Header.js";
+import Footer from "./Footer.js";
+import Note from "./Note.js";
+
+import Textarea from "./txtarea.js";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="App">
+      <Header className="header" />
+      <Textarea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header>
+      <h1 className="header">Keep Notes</h1>
+    </header>
+  );
+}
+export default Header;
